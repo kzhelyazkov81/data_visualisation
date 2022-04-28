1473e3f62dbe3e14739ce72adfd6ca455cb55283
kzhelyazkov81 <kzhelyazkov81@gmail.com>
2022-04-28 15:28:10 +0300

    update

--- die_visual.py
+++ die_visual.py
@@ -1,5 +1,6 @@
 from die import Die
 from plotly.graph_objs import Bar, Layout
+from plotly import offline
 
 # Create a D6.
 die = Die()
@@ -16,4 +17,9 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+x_values = list(range(1, die.num_sides+1 ))
+data = [Bar(x=x_values, y=frequencies)]
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Results of rolling on D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
